227f1db926a624f0cfe24db8a9c370bb2ed6a695
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-01-24 18:10:31 +0100

    add comments

--- src/demos/1_Base_Component/Demo1Component.jsx
+++ src/demos/1_Base_Component/Demo1Component.jsx
@@ -1,10 +1,12 @@
 // Import fichier css classique
 import './Demo1.css';
 
-// Import fichier css module
+// Import fichier module css
+// Quand on importe un module, on a accès un objet dans lequel il y aura toutes les classes
 import style from './Demo1.module.css';
 
 export const Demo1Component = (props) => {
+    //#region Explications Props
     //props est un objet dans lequel il y aura des propriétés qui porteront le nom de ce qui aura été envoyé lors de l'utilisation du composant
     // <Demo1Component name="HTML" type="Front-End"/>
     
@@ -16,6 +18,8 @@ export const Demo1Component = (props) => {
     return (
         <>
         <p className={style['grey-text']}>Bienvenue au cours de {name} qui est du {type}</p>
+
+        {/* Utilisation du css classique */}
         <p className="grey-text">Difficulté : {difficulty}/5</p>
         </>
     );

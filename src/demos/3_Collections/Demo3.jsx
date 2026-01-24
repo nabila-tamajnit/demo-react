902c79effd1ab378fb775f8f265fc17bf74aeb36
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-01-24 18:14:55 +0100

    start demo3 Collections

---
+++ src/demos/3_Collections/Demo3.jsx
@@ -0,0 +1,34 @@
+
+export const Demo3 = () => {
+
+    const trainers = [
+        { 
+            id : 1, 
+            firstname : 'Aude',
+            lastname : 'Beurivé',
+            gender : 'f',
+            vacations : false,
+            hobbies : [ 'Dessin', 'Tricot', 'Soup' ]
+        },
+        { 
+            id : 2,
+            firstname : 'Aurélien',
+            lastname : 'Strimelle',
+            gender : 'm',
+            vacations : false,
+            hobbies : [ 'Randonnée', 'Jeux de société']
+        },
+        { 
+            id : 3,
+            firstname : 'Quentin',
+            lastname : 'Geerts',
+            gender : 'm',
+            vacations : true,
+            hobbies : [ 'Mangas', 'Jeux Vidéos' ]
+        }
+    ];
+
+    return (
+        <div>TODO AFFICHER TABLEAU</div>
+    )
+}
